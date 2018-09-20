ed9bc46487d2a3376db0e03740261a0c35a6ba49
MrK-0day <hack.mr.k.2712@gmail.com>
2018-09-20 17:21:26 +0700

    tuan-20/9

---
+++ client/src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+// Import Config
+import './config/configPage'
+
+// Import Screen
+import Home from './screens/home/Home'
+
+ReactDOM.render(<Home />, document.getElementById('root'))
--- client/src/screens/home/Home.tsx
+++ client/src/screens/home/Home.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import * as React from 'react'
 
 import Test from './test'
 
-class Home extends React.Component {
+class Home extends React.Component<any, any> {
   render () {
     return (
       <div>
